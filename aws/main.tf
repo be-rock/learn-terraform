2b68ec4b90a8d5e5b5405c45a6c28b16484bb191
be-rock <brock.secondary@gmail.com>
2021-06-06 19:23:45 -0500

    add vars and outputs

--- aws/main.tf
+++ aws/main.tf
@@ -1,13 +1,18 @@
 terraform {
+#   backend "remote" {
+    # organization = "<ORG_NAME>"
+    # workspaces {
+    #   name = "Example-Workspace"
+    # }
+#   }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.27"
     }
   }
-
-  required_version = ">= 0.14.9"
 }
+
 
 provider "aws" {
   profile = "default"
@@ -15,11 +20,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = var.instance_name
   }
 }
 
